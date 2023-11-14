991ac4953e3300da867cca4952345d7c27af1de4
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-14 17:33:49 +0100

    completare con bonus e interfaccia. logica baseok

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-/*CONSEGNA
-Creare un carosello come nella foto allegata, partendo un array di oggetti letterali.
-Ogni oggetto avrà le seguenti proprietà:
- url dell’immagine
-titolo
-descrizione
-MILESTONE #0
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-MILESTONE #1
-Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-MILESTONE #2
-Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+/* l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
 BONUS #1
 Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
 BONUS #2
